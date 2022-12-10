d4fd70030d3708af4437a5d04956c0ca4fb659f3
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-12-10 13:00:34 +0200

    Set up `react-bootstrap`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
@@ -15,6 +16,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p>Hello, ASMI!</p>
+        <p>
+          <Button variant="primary">Click me</Button>
+        </p>
 
         <p className={styles.description}>
           Get started by editing{" "}
