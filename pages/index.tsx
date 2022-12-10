d820edbd880818a63dddefa71adea6c634c110b1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-12-10 15:43:25 +0200

    Run formatter on code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
   return (
