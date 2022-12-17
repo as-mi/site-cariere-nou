46fd5c9a94b1b2ced7b27a64f69e5d765500f095
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2022-12-17 20:06:34 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Script from 'next/script';
 import type { AppProps } from "next/app";
-import { config } from '@fortawesome/fontawesome-svg-core';
+import { config } from "@fortawesome/fontawesome-svg-core";
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
