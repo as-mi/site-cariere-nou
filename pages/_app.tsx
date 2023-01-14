fdbce59f70aa3dbadf812b388c1d6b89e599bbea
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-14 22:04:37 +0200

    Set up `next-i18next` and add some content to home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { appWithTranslation } from "next-i18next";
 
-export default function App({
+const App: React.FC<AppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
-}
+};
+
+export default appWithTranslation(App);
