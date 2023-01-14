6b3b14c9076213be7af43ef58f1dd7a9e34f136a
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-14 20:54:16 +0200

    Remove existing styling before switching to Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({
   Component,
