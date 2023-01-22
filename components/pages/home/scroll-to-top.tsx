236c8ed189c6c206ff2a60278f59ccb2224228d1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-22 23:26:42 +0200

    Add accessible label to "scroll-to-top" button

--- components/pages/home/scroll-to-top.tsx
+++ components/pages/home/scroll-to-top.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "next-i18next";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const ScrollToTopButton: React.FC = () => {
+  const { t } = useTranslation("common");
+
   const handleClick = () => {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
   };
@@ -11,7 +15,11 @@ const ScrollToTopButton: React.FC = () => {
       onClick={handleClick}
       className="fixed bottom-4 right-4 rounded-full bg-green-800 p-2 text-white"
     >
-      <FontAwesomeIcon icon={faArrowUp} className="h-6 w-6" />
+      <FontAwesomeIcon
+        icon={faArrowUp}
+        title={t("scrollToTop") || undefined}
+        className="h-6 w-6"
+      />
     </button>
   );
 };
