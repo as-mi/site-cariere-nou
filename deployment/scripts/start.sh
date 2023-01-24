0ba875ffa5645503056785adcca2757627470242
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-24 14:22:50 +0200

    Use Node.js v16 in production due to Prisma issue

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -7,7 +7,9 @@ export NVM_DIR=$HOME/.nvm
 source $NVM_DIR/nvm.sh
 
 # Activate the desired Node.js version
-nvm use --lts
+# TODO: switch to LTS once https://github.com/prisma/prisma/issues/10649#issuecomment-1249209025 gets fixed
+# Prisma currently leads to segmentation faults when trying to connect to PostgreSQL under Node 18
+nvm use 16
 
 # Start the Next.js production server
 npm run start
