d4cd96166498ab4ffe0bea4db41881ec41494898
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-28 12:33:50 +0200

    Set up automatic unit testing using Jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Path to Next.js app root directory
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Only look for test files in the `./src/__tests__` subdirectory
+  testMatch: ["<rootDir>/src/__tests__/**/*.[jt]s?(x)"],
+  // Run the setup file to import additional useful packages
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    // Configure our custom path alias for `~`
+    "~/(.*)$": "<rootDir>/src/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
