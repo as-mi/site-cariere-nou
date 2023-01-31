90dd74c366a7ef7282074bdc2682990d9cb4ad10
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-01-31 20:23:55 +0200

    Add admin edit user route

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,7 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 
-import { appRouter } from "~/server/routers/_app";
-import { createContext } from "~/server/context";
+import { appRouter } from "~/api/trpc/routers/_app";
+import { createContext } from "~/api/trpc/context";
 
 /**
  * Next.js API handler which forwards requests to tRPC.
