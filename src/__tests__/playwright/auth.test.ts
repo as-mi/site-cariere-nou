67cdddc082017b4bf43189177cfaeda4e2ec6124
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-02-04 11:23:10 +0200

    Try making registration E2E test more robust

--- src/__tests__/playwright/auth.test.ts
+++ src/__tests__/playwright/auth.test.ts
@@ -1,42 +1,48 @@
 import { test, expect } from "@playwright/test";
 
 test("can create new account using e-mail and password", async ({ page }) => {
+  // Visit the home page
   await page.goto("/");
 
+  // Click on the "My account" button
   const myProfileLink = await page.waitForSelector(`text=Contul meu`);
-
   await myProfileLink.click();
 
+  // Wait for the page to load
   await page.waitForSelector(`text=Intră în cont`);
 
+  // Go to the e-mail login page
   await page.click(`text=Autentificare prin e-mail`);
 
-  await page.waitForSelector(`input[type=submit]:is(:text("Autentifică-te"))`);
+  await page.waitForSelector(`input[type=submit]:text("Autentifică-te")`);
 
+  // Choose the option to create a new account
   await page.click(`text=Nu ai încă cont?`);
 
-  const registerButton = await page.waitForSelector(
-    `input[type=submit]:is(:text("Înregistrează-te"))`
-  );
+  await page.waitForSelector(`input[type=text][name=name]`);
 
+  // Generate some fake user data
   const nonce = Math.floor(Math.random() * 65536);
   const email = `example${nonce}@example.com`;
   const name = "Example User";
   const password = "12345678";
 
+  // Fill in the registration form
   await page.fill(`[name=email]`, email);
   await page.fill(`[name=name]`, name);
   await page.fill(`[name=password]`, password);
   await page.fill(`[name=passwordConfirmation]`, password);
   await page.check(`[name=consent]`);
 
+  const registerButton = await page.waitForSelector(
+    `input[type=submit]:is(:text("Înregistrează-te"))`
+  );
   await registerButton.click();
 
+  // Wait for the registration to finish
   await page.waitForLoadState("networkidle");
 
-  const mainElement = await page.waitForSelector("main");
-
-  expect(await mainElement.textContent()).toContain(
-    "Înregistrarea s-a efectuat cu succes!"
-  );
+  // Check that a success message is displayed
+  const successMessage = "Înregistrarea s-a efectuat cu succes!";
+  await page.waitForSelector(`h2:text("${successMessage}")`);
 });
