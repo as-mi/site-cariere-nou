a8a4ce6497f861bc7f85f89069cacbe2e7097163
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-02-23 18:07:25 +0200

    Replace `noreferrer` with `noopener` for external links

--- src/components/common/external-link.tsx
+++ src/components/common/external-link.tsx
@@ -7,7 +7,7 @@ const ExternalLink: React.FC<ExternalLinkProps> = ({ href, children }) => (
   <a
     href={href}
     target="_blank"
-    rel="noreferrer"
+    rel="noopener"
     className="cursor-pointer text-green-800 hover:text-green-600"
   >
     {children}
