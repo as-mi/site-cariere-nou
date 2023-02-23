0e1de799052304f89053738b6ec2157f4cf2b1d2
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-02-24 00:56:19 +0200

    Ignore ESLint warning related to `rel="noreferrer"`

--- src/components/common/external-link.tsx
+++ src/components/common/external-link.tsx
@@ -4,6 +4,8 @@ type ExternalLinkProps = {
 };
 
 const ExternalLink: React.FC<ExternalLinkProps> = ({ href, children }) => (
+  // We already use `noopener` for security, we don't want to switch to `noreferrer` for SEO reasons.
+  // eslint-disable-next-line react/jsx-no-target-blank
   <a
     href={href}
     target="_blank"
