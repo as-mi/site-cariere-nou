497c4d8feb810d0cf513177811a824cb2d74e3dc
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-02-24 10:04:47 +0200

    Increase scroll-to-top button z-index and add drop-shadow

--- src/components/pages/home/scroll-to-top.tsx
+++ src/components/pages/home/scroll-to-top.tsx
@@ -13,7 +13,7 @@ const ScrollToTopButton: React.FC = () => {
   return (
     <button
       onClick={handleClick}
-      className="fixed bottom-4 right-4 rounded-full bg-green-800 p-2 text-white"
+      className="fixed bottom-4 right-4 z-50 rounded-full bg-green-800 p-2 text-white drop-shadow-md"
     >
       <span title={t("scrollToTop") || undefined}>
         <FontAwesomeIcon icon={faArrowUp} className="h-6 w-6" />
