a743956fddd93d5b8723f88e2442145a70044ce6
Pepi100 <radunedelcu02@gmail.com>
2023-02-26 00:45:55 +0200

    Fixed Database

---
+++ prisma/migrations/20230225224309_urls/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "facebookUrl" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "instagramUrl" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "linkedinUrl" TEXT NOT NULL DEFAULT '';
