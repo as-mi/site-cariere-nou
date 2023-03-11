081850fe3bf429e329fc7acff389beb059d4d9a1
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-03-11 13:01:08 +0200

    Update admin interface button default style

--- src/components/pages/admin/common/button.tsx
+++ src/components/pages/admin/common/button.tsx
@@ -15,7 +15,7 @@ const Button = <C extends React.ElementType = "button">({
   return (
     <Element
       {...otherProps}
-      className="inline-block rounded-md bg-blue-600 py-2 px-3 hover:bg-blue-500 active:bg-blue-400 active:text-gray-800"
+      className="inline-block rounded-md border-2 border-blue-600 bg-black py-2 px-3 hover:bg-blue-600 active:border-blue-400"
     >
       {children}
     </Element>
