6d81309b7ec9c335fb9b3d09f653eed2c2f12738
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-02-23 00:07:37 +0200

    Switch to using Node.js LTS version in production

--- deployment/scripts/start.sh
+++ deployment/scripts/start.sh
@@ -7,9 +7,7 @@ export NVM_DIR=$HOME/.nvm
 source $NVM_DIR/nvm.sh
 
 # Activate the desired Node.js version
-# TODO: switch to LTS once https://github.com/prisma/prisma/issues/10649#issuecomment-1249209025 gets fixed
-# Prisma currently leads to segmentation faults when trying to connect to PostgreSQL under Node 18
-nvm use 16
+nvm use --lts
 
 # Start the Next.js production server
 npm run start
